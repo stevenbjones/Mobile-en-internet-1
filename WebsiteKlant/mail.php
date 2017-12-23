674f817eb63ec22f1950e363443e0beb3597a844
stevenbjones <32386913+stevenbjones@users.noreply.github.com>
2017-12-23 22:12:32 +0100

    finale versie

--- WebsiteKlant/mail.php
+++ WebsiteKlant/mail.php
@@ -2,7 +2,7 @@
 $to      = 'stevenbjones@hotmail.com';
 $subject = $_POST['Onderwerp'];
 $message = $_POST['Vraag'];
-$headers = 'From: $_POST['Mail']' . "\r\n" .
+$headers = 'From: $_POST['Email']' . "\r\n" .
     'Reply-To: webmaster@example.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
